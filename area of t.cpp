4c4dc65f78c670fe476212281b979b0d0eba9fb1
Rohit <rohithakare777@gmail.com>
2021-09-04 13:50:12 +0530

    message

---
+++ area of t.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+	int l,b,a ;
+	printf("enter the values : ");
+	scanf("%d%d",&l,&b);
+	a=0.5*l*b;
+	printf("\n length is : %d",l);
+	printf("\n breadth is : %d",b);
+	printf("\n Area is : %d",a);
+}
